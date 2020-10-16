a4878214b1898d263f8c86615a617c169632a7db
Администратор <@DESKTOP-PHORFLN>
2020-10-16 20:03:42 +0300

    123

--- ResearchTeam.cs
+++ ResearchTeam.cs
@@ -1,15 +1,21 @@
 ﻿using Microsoft.TeamFoundation.Work.WebApi;
+using Microsoft.VisualBasic;
 using System;
 
 namespace Практика6.Вариант_3
 {
     class ResearchTeam
     {
+        public enum TimeFrame 
+        {
+            Year = 1, TwoYears = 2, Long = 3 
+        };
         private string researchtopic;
         private string organization;
         private int number;
         private TimeFrame time;
         private Paper[] list;
+        
 
         public ResearchTeam(string _reasearchtopic, string _organization, int _number, TimeFrame _time, Paper[] _list)
         {
@@ -25,7 +31,8 @@ namespace Практика6.Вариант_3
             researchtopic = "Newton third law";
             organization = "World organization of physicists";
             number = 1;
-            time = new TimeFrame(1);
+            TimeFrame time = TimeFrame.Year;
+            list = new Paper[5];
             for (int i = 0; i < 5; ++i)
             {
                 list[i] = new Paper();
